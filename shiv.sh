9aaeafc5257191a8a74829b4649a35a07728c9fe
raghukultilak <158991858+raghukultilak@users.noreply.github.com>
2024-02-18 14:22:08 +0530

    feat: Create shiv.sh file
    Create shiv.sh file

---
+++ shiv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#this is my first script for person description.
+
+name=avinash 
+age=32
+city=gwalior
+state=madhya pradesh
+country=india
+
+echo "my name is $name"
+echo "my age is $age"
+echo "i live at $city in $state "
+echo "my citizenship is from $country"
